7063f1005bfd9fb1ddb536f0c2549f7fa0c303d6
NguyenLe1605 <lhdnguyen2002@gmail.com>
2023-03-28 18:42:33 +0700

    Add cors to server

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const express = require('express');
 const morgan = require('morgan');
 
 const app = express();
+const cors = require('cors');
 const Person = require('./models/person');
 
+app.use(cors());
 app.use(express.json());
 app.use(express.static('build'));
 morgan.token('data', (request) => JSON.stringify(request.body));
